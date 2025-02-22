9343efd208c0ca4537c79421f738d5e7c75254a5
stellaYYX <104099845+stellaYYX@users.noreply.github.com>
2025-02-22 09:53:19 +0800

    Add files via upload

--- datasets/graph_2011/data_formatting.m
+++ datasets/graph_2011/data_formatting.m
@@ -9,74 +9,75 @@
 
 
 %% Load the data
-arrayNumber = 1;  % between 1 and 5
-load(sprintf('./array files/array_%d.mat', arrayNumber));
-nNeur = n_neur;
-nRpts = n_trial;
-nOris = n_ori;
-ori = ori;
-oriAxis = ori_tot;
-stsAll = spk_times;
+for arrayNumber = 1:5
+    load(sprintf('./array files/array_%d.mat', arrayNumber));
+    nNeur = n_neur;
+    nRpts = n_trial;
+    nOris = n_ori;
+    ori = ori;
+    oriAxis = ori_tot;
+    stsAll = spk_times;
 
 
-%% Compute spike counts for each trial, orientation and neuron
-scAll = zeros(nNeur,nRpts,nOris);
-for jori = 1:nOris
-    idx = find(ori == oriAxis(jori));
-    scAll(:,:,jori) = cellfun(@length, stsAll(:,idx));
-end
-scAll = permute(scAll,[3,1,2]);  % permute to be [ori,neuron,rpt] 
-
-% If true, average [0 180] with responses from [180 360]
-REFLECT = false;  
-if REFLECT
-    % average over direction
-    scRef = zeros(nOris/2,nNeur,nRpts*2);
-    scRef(:,:,1:nRpts) = scAll(1:nOris/2,:,:);
-    scRef(:,:,nRpts+1:2*nRpts) = scAll(nOris/2+1:end,:,:);
-    nOris = nOris/2;
-    nRpts = nRpts*2;
-    scAll = scRef;
-    oriAxis = oriAxis(1:nOris);
-end
-
-% Reshape to unify formatting with other datasets
-scAll = permute(scAll,[2,3,1]);
-
-
-%% Visualize tuning profiles across different trials of example neurons
-f = figure();
-f.Position = [0 0 1500 900];
-counter = 1
-for i=1:10
-    for j=1:5
-        subplot(5,10,counter);
-        plot(oriAxis,squeeze(scAll(i,j,:)));
-        counter = counter + 1;
+    %% Compute spike counts for each trial, orientation and neuron
+    scAll = zeros(nNeur,nRpts,nOris);
+    for jori = 1:nOris
+        idx = find(ori == oriAxis(jori));
+        scAll(:,:,jori) = cellfun(@length, stsAll(:,idx));
     end
-end
+    scAll = permute(scAll,[3,1,2]);  % permute to be [ori,neuron,rpt] 
 
-
-%% Visualize mean tuning profiles of example neurons averaged across trial folds
-f = figure();
-f.Position = [0 0 1500 300];
-counter = 1
-for i=1:10
-    for split=1:2
-        trials = [1:25] + 25*(split-1);
-        subplot(2,10,counter);
-        plot(oriAxis,squeeze(mean(scAll(i,trials,:),2)));
-        counter = counter + 1;
+    % If true, average [0 180] with responses from [180 360]
+    REFLECT = false;  
+    if REFLECT
+        % average over direction
+        scRef = zeros(nOris/2,nNeur,nRpts*2);
+        scRef(:,:,1:nRpts) = scAll(1:nOris/2,:,:);
+        scRef(:,:,nRpts+1:2*nRpts) = scAll(nOris/2+1:end,:,:);
+        nOris = nOris/2;
+        nRpts = nRpts*2;
+        scAll = scRef;
+        oriAxis = oriAxis(1:nOris);
     end
+
+    % Reshape to unify formatting with other datasets
+    scAll = permute(scAll,[2,3,1]);
+
+
+    %% Visualize tuning profiles across different trials of example neurons
+    f = figure();
+    f.Position = [0 0 1500 900];
+    counter = 1
+    for i=1:10
+        for j=1:5
+            subplot(5,10,counter);
+            plot(oriAxis,squeeze(scAll(i,j,:)));
+            counter = counter + 1;
+        end
+    end
+
+
+    %% Visualize mean tuning profiles of example neurons averaged across trial folds
+    f = figure();
+    f.Position = [0 0 1500 300];
+    counter = 1
+    for i=1:10
+        for split=1:2
+            trials = [1:25] + 25*(split-1);
+            subplot(2,10,counter);
+            plot(oriAxis,squeeze(mean(scAll(i,trials,:),2)));
+            counter = counter + 1;
+        end
+    end
+
+
+    %% Save to output
+
+    % Output format: 
+    %   spike_cnt -> nNeuron x nTrial x nOri
+    %   orientations -> 1 x nOri
+    orientations = oriAxis/360*2*pi;
+    spike_cnt = scAll;
+    save(sprintf('./Data Formatted/Graph_0%d',arrayNumber),'spike_cnt','orientations');
 end
-
-
-%% Save to output
-
-% Output format: 
-%   spike_cnt -> nNeuron x nTrial x nOri
-%   orientations -> 1 x nOri
-orientations = oriAxis;
-spike_cnt = scAll;
-save(sprintf('./Data Formatted/Graph_0%d',arrayNumber),'spike_cnt','orientations');
 
